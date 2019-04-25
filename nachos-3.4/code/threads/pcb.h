658b3d9e8b578ec9bf8b2f23463933868697baec
Blue <39703705+dinhletrieuduong@users.noreply.github.com>
2019-04-25 18:06:55 +0700

    Create pcb.h

---
+++ nachos-3.4/code/threads/pcb.h
@@ -0,0 +1,47 @@
+#ifndef PCB_H
+#define PCB_H
+
+#include "thread.h"
+#include "synch.h"
+
+class PCB {
+private:
+	Semaphore	*joinsem;	//semaphore cho qua trinh join
+	Semaphore	*exitsem;	//semaphore cho qua trinh exit
+	Semaphore	*mutex;		// semaphore cho quá trình truy xuất đọc quyền
+	
+	int		exitcode;	
+	int		numwait;		//so tien trinh da join
+
+	int pid;
+	Thread *thread;
+public:
+	int 	parentID;		//ID cua tien trinh cha
+	PCB();
+	PCB(int id); 			// contructor
+	~PCB();					// deconstructor
+	
+	// nạp chương trình có tên lưu trong biến filename và processID là pid
+	int Exec(char *filename, int pid); // Tạo 1 thread mới có tên là filename và process là pid
+	int GetID();				// Trả về ProcessID của tiến trình gọi thực hiện
+	int GetNumWait();			// Trả về số lượng tiến trình chờ
+	
+	void JoinWait();			// 1. Tiến trình cha đợi tiến trình con kết thúc
+	void ExitWait();			// 4. Tiến trình con kết thúc
+	void JoinRelease(); 		// 2. Báo cho tiến trình cha thực thi tiếp
+	void ExitRelease(); 		// 3. Cho phép tiến trình con kết thúc
+	
+	void IncNumWait();			// Tăng số tiến trình chờ
+	void DecNumWait();			// Giảm số tiến trình chờ
+	
+	void SetExitCode(int ec);	// Đặt exitcode của tiến trình
+	int GetExitCode();			// Trả về exitcode
+		
+	void SetFileName(char* fn);	// Đặt tên của tiến trình
+	char* GetFileName();		// Trả về tên của tiến trình
+};
+
+//*********************************************************************************************************************
+void StartProcess(int);
+
+#endif
